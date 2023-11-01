ab3c78f20e4a41bb8950a1774632f296968520cd
Łukasz Bednarski <lukasz@bednarski.me>
2023-11-01 18:36:34 +0100

    Day 23 & 25, 2015

---
+++ src/aoc2015/day23.rs
@@ -0,0 +1,116 @@
+use std::fmt::{Display, Formatter, Result};
+
+use aoc::aoc_input;
+
+#[derive(Debug)]
+enum Instructions {
+    Hlf(usize),
+    Tpl(usize),
+    Inc(usize),
+    Jmp(i64),
+    Jie(usize, i64),
+    Jio(usize, i64),
+    Invalid,
+}
+
+impl<S: Into<String>> From<S> for Instructions {
+    fn from(value: S) -> Self {
+        let value = value.into();
+        let parts = value
+            .split(' ')
+            .map(|x| x.strip_suffix(',').unwrap_or(x))
+            .collect::<Vec<&str>>();
+        let register = (parts[1].chars().nth(0).unwrap() as u8).saturating_sub(b'a') as usize;
+        let as_num = |x: &str| x.parse::<i64>().unwrap();
+
+        match parts[0] {
+            "hlf" => Self::Hlf(register),
+            "tpl" => Self::Tpl(register),
+            "inc" => Self::Inc(register),
+            "jmp" => Self::Jmp(as_num(parts[1])),
+            "jie" => Self::Jie(register, as_num(parts[2])),
+            "jio" => Self::Jio(register, as_num(parts[2])),
+            _ => Self::Invalid,
+        }
+    }
+}
+
+struct Program<'a> {
+    pos: i64,
+    registers: Vec<i64>,
+    tape: &'a Vec<Instructions>,
+}
+
+impl<'a> Display for Program<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{}", self.registers.get(1).unwrap_or(&0))
+    }
+}
+
+impl<'a> Program<'a> {
+    fn new(tape: &'a Vec<Instructions>) -> Self {
+        Self {
+            pos: 0,
+            registers: vec![0, 0],
+            tape,
+        }
+    }
+
+    fn eat(&mut self) -> Option<()> {
+        match self.tape.get(self.pos as usize) {
+            Some(Instructions::Hlf(reg)) => {
+                self.registers[*reg] /= 2;
+                self.pos += 1;
+            }
+            Some(Instructions::Tpl(reg)) => {
+                self.registers[*reg] *= 3;
+                self.pos += 1;
+            }
+            Some(Instructions::Inc(reg)) => {
+                self.registers[*reg] += 1;
+                self.pos += 1;
+            }
+            Some(Instructions::Jmp(off)) => self.pos += *off,
+            Some(Instructions::Jie(reg, off)) => match self.registers[*reg] % 2 == 0 {
+                true => self.pos += *off,
+                false => self.pos += 1,
+            },
+            Some(Instructions::Jio(reg, off)) => match self.registers[*reg] == 1 {
+                true => self.pos += *off,
+                false => self.pos += 1,
+            },
+            _ => {
+                return None;
+            }
+        }
+        Some(())
+    }
+
+    fn run(&mut self) {
+        while self.eat().is_some() {}
+    }
+
+    fn reset(&mut self, registers: &[i64]) {
+        self.pos = 0;
+        self.registers = registers.to_owned();
+    }
+}
+
+fn main() {
+    let data = aoc_input!(2015, 23).unwrap();
+    let tape = data
+        .lines()
+        .filter(|x| !x.is_empty())
+        .map(Instructions::from)
+        .collect::<Vec<Instructions>>();
+
+    // Part I
+    let mut prog = Program::new(&tape);
+    prog.run();
+    println!("{}", prog);
+
+    // Part I
+    prog.reset(&[1, 0]);
+    prog.run();
+    println!("{}", prog);
+}
---
+++ src/aoc2015/day25.rs
@@ -0,0 +1,18 @@
+fn get_term(x0: u64, n: u64) -> u64 {
+    let a: u64 = 252533;
+    let b: u64 = 33554393;
+    let c = (1..n).fold(1, |acc, _| acc * a % b);
+    x0 * c % b
+}
+
+fn iloc(row: u64, col: u64) -> u64 {
+    let n = row + col - 1;
+    col + n * (n - 1) / 2
+}
+
+fn main() {
+    let (row, col) = (3010, 3019);
+    let x0 = 20151125;
+    let i = iloc(row, col);
+    println!("{}", get_term(x0, i));
+}
