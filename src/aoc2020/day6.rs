2e5ec426b6ea167d4fbeb61f1ef72f9fbb5a7370
Łukasz Bednarski <lukasz@bednarski.me>
2023-11-09 11:31:31 +0100

    Improve Day 6, 2020

--- src/aoc2020/day6.rs
+++ src/aoc2020/day6.rs
@@ -1,39 +1,37 @@
 use aoc::aoc_input;
 use itertools::Itertools;
-use rustc_hash::FxHashSet;
 use std::borrow::Borrow;
+use std::collections::HashSet;
+use std::hash::Hash;
 
-fn intersect<I>(sets: I) -> FxHashSet<char>
+fn intersect<I, T>(sets: I) -> HashSet<T>
 where
-    I: IntoIterator + Clone,
-    I::Item: Borrow<FxHashSet<char>> + Clone,
+    I: IntoIterator,
+    I::Item: Borrow<HashSet<T>>,
+    T: Eq + Copy + Hash,
 {
-    let first = match sets.clone().into_iter().next() {
-        Some(val) => val.clone(),
-        None => return FxHashSet::default(),
-    };
+    let mut sets = sets.into_iter();
+    let mut result: HashSet<T> = sets
+        .next()
+        .map(|s| s.borrow().iter().copied().collect())
+        .unwrap_or_default();
 
-    first
-        .borrow()
-        .iter()
-        .filter(move |elem| {
-            sets.clone()
-                .into_iter()
-                .all(|set| set.borrow().contains(elem))
-        })
-        .cloned()
-        .collect()
+    for set in sets {
+        result.retain(|elem| set.borrow().contains(elem));
+    }
+
+    result
 }
 
 fn main() {
     let data = aoc_input!(2020, 6).unwrap();
-    let answers: Vec<Vec<FxHashSet<char>>> = data
+    let answers: Vec<Vec<HashSet<char>>> = data
         .trim_end()
         .split("\n\n")
         .map(|lines| {
             lines
                 .lines()
-                .map(|line| line.chars().collect::<FxHashSet<char>>())
+                .map(|line| line.chars().collect::<HashSet<char>>())
                 .collect()
         })
         .collect();
