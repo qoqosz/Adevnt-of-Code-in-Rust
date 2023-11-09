38c69b41155c8ac4dbde0e20079360c249202b9f
Łukasz Bednarski <lukasz@bednarski.me>
2023-11-09 14:28:47 +0100

    Restructure day 7, 2020

--- src/aoc2020/day7.rs
+++ src/aoc2020/day7.rs
@@ -2,57 +2,79 @@ use aoc::aoc_input;
 use regex::Regex;
 use rustc_hash::FxHashMap;
 
-type Bag<'a> = FxHashMap<&'a str, Vec<(usize, &'a str)>>;
-
-fn contains(bag: &Bag, color: &str, target_color: &str) -> bool {
-    if color.contains(target_color) {
-        return true;
-    }
-    match bag.get(color) {
-        Some(inside) => inside.iter().any(|(_, c)| contains(bag, c, target_color)),
-        _ => false,
-    }
+struct Bag<'a> {
+    container: FxHashMap<&'a str, Vec<(usize, &'a str)>>,
+    target_color: Option<&'a str>,
 }
 
-fn capacity(bag: &Bag, color: &str) -> usize {
-    match bag.get(color) {
-        Some(inside) => inside.iter().map(|(n, c)| n + n * capacity(bag, c)).sum(),
-        _ => 0,
+impl<'a> Bag<'a> {
+    fn from_input(data: &'a str) -> Self {
+        let re: Regex = Regex::new(r"(\d+) (.*?) bag").unwrap();
+
+        let container: FxHashMap<&'a str, Vec<(usize, &'a str)>> = data
+            .lines()
+            .filter_map(|line| {
+                let (out_color, in_color) = line.trim().split_once(" bags contain ").unwrap();
+                if in_color.starts_with("no other") {
+                    None
+                } else {
+                    let colors = re
+                        .captures_iter(line)
+                        .map(|d| {
+                            (
+                                d.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+                                d.get(2).unwrap().as_str(),
+                            )
+                        })
+                        .collect::<Vec<_>>();
+                    Some((out_color, colors))
+                }
+            })
+            .collect();
+
+        Self {
+            container,
+            target_color: None,
+        }
+    }
+
+    fn set_color(&mut self, color: &'a str) {
+        self.target_color = Some(color);
+    }
+
+    fn contains(&self, color: &'a str) -> bool {
+        if color.contains(self.target_color.unwrap()) {
+            return true;
+        }
+        match self.container.get(color) {
+            Some(inside) => inside.iter().any(|(_, c)| self.contains(c)),
+            _ => false,
+        }
+    }
+
+    fn count(&self) -> usize {
+        self.container.keys().filter(|c| self.contains(c)).count() - 1
+    }
+
+    fn capacity(&self) -> usize {
+        fn _capacity(bag: &Bag, color: &str) -> usize {
+            match bag.container.get(color) {
+                Some(inside) => inside.iter().map(|(n, c)| n + n * _capacity(bag, c)).sum(),
+                _ => 0,
+            }
+        }
+        _capacity(self, self.target_color.unwrap())
     }
 }
 
 fn main() {
     let data = aoc_input!(2020, 7).unwrap();
-    let target_color = "shiny gold";
-    let re: Regex = Regex::new(r"(\d+) (.*?) bag").unwrap();
-    let bag: Bag = data
-        .lines()
-        .filter_map(|line| {
-            let (out_color, in_color) = line.trim().split_once(" bags contain ").unwrap();
-            if in_color.starts_with("no other") {
-                None
-            } else {
-                let colors = re
-                    .captures_iter(line)
-                    .map(|d| {
-                        (
-                            d.get(1).unwrap().as_str().parse::<usize>().unwrap(),
-                            d.get(2).unwrap().as_str(),
-                        )
-                    })
-                    .collect::<Vec<_>>();
-                Some((out_color, colors))
-            }
-        })
-        .collect();
+    let mut bag = Bag::from_input(&data);
+    bag.set_color("shiny gold");
 
     // Part I
-    let n = bag
-        .keys()
-        .filter(|c| contains(&bag, c, target_color))
-        .count();
-    println!("{}", n - 1);
+    println!("{}", bag.count());
 
     // Part II
-    println!("{}", capacity(&bag, target_color));
+    println!("{}", bag.capacity());
 }
