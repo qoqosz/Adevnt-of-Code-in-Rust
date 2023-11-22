361cae28017605ddfd89d8f4781f48271c90de5e
Łukasz Bednarski <lukasz@bednarski.me>
2023-11-22 09:05:38 +0100

    refactor cli

--- src/bin/download.rs
+++ src/bin/download.rs
@@ -1,79 +1,25 @@
 // TODO: File doc
-use aoc::{aoc_input, date::get_current_year};
+use aoc::aoc_input;
+use aoc::cli::{Args, ArgsError};
+use std::process::ExitCode;
 
-const HELP: &str = "\
-USAGE: download [FLAGS] [OPTIONS] -d <DAY>
-
-FLAGS:
-  -h, --help    Prints help information
-
-OPTIONS:
-  -y YEAR       Sets AoC year; use current year if not provided
-  -d DAY        Sets AoC day
-";
-
-#[derive(Debug, Clone)]
-enum ArgsError {
-    Help,
-    Error(String),
-}
-
-impl std::fmt::Display for ArgsError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Help => write!(f, "{}", HELP),
-            Self::Error(msg) => write!(f, "Error while parsing arguments: {}", msg),
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Args {
-    pub day: u16,
-    pub year: u16,
-}
-
-impl TryFrom<pico_args::Arguments> for Args {
-    type Error = ArgsError;
-
-    fn try_from(mut args: pico_args::Arguments) -> Result<Self, Self::Error> {
-        if args.contains(["-h", "--help"]) {
-            return Err(ArgsError::Help);
-        }
-
-        let day = match args.value_from_str("-d") {
-            Ok(day) => day,
-            Err(e) => {
-                return Err(ArgsError::Error(format!("{e}")));
-            }
-        };
-
-        Ok(Args {
-            day,
-            year: args
-                .value_from_str("-y")
-                .unwrap_or_else(|_| get_current_year()),
-        })
-    }
-}
-
-fn main() -> std::process::ExitCode {
+fn main() -> ExitCode {
     let pargs = pico_args::Arguments::from_env();
     let args = match Args::try_from(pargs) {
         Ok(args) => args,
         Err(help @ ArgsError::Help) => {
             eprintln!("{help}");
-            return std::process::ExitCode::SUCCESS;
+            return ExitCode::SUCCESS;
         }
         Err(err) => {
             eprintln!("{err}");
-            return std::process::ExitCode::FAILURE;
+            return ExitCode::FAILURE;
         }
     };
 
     if let Err(err) = aoc_input!(args.year, args.day) {
-        eprintln!("{}", err);
-        return std::process::ExitCode::FAILURE;
+        eprintln!("{err}");
+        return ExitCode::FAILURE;
     }
-    std::process::ExitCode::SUCCESS
+    ExitCode::SUCCESS
 }
---
+++ src/lib/cli.rs
@@ -0,0 +1,69 @@
+use crate::date::get_current_year;
+
+pub const HELP: &str = "\
+USAGE: {} [FLAGS] [OPTIONS] -d <DAY>
+
+FLAGS:
+  -h, --help    Prints help information
+
+OPTIONS:
+  -y YEAR       Sets AoC year; use current year if not provided
+  -d DAY        Sets AoC day
+";
+
+fn prog() -> Option<String> {
+    std::env::current_exe()
+        .ok()?
+        .file_name()?
+        .to_str()?
+        .to_string()
+        .into()
+}
+
+#[derive(Debug, Clone)]
+pub enum ArgsError {
+    Help,
+    Error(String),
+}
+
+impl std::fmt::Display for ArgsError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Help => {
+                let help = HELP.replace("{}", &prog().unwrap());
+                write!(f, "{}", help)
+            }
+            Self::Error(msg) => write!(f, "Error while parsing arguments: {}", msg),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Args {
+    pub day: u16,
+    pub year: u16,
+}
+
+impl TryFrom<pico_args::Arguments> for Args {
+    type Error = ArgsError;
+
+    fn try_from(mut args: pico_args::Arguments) -> Result<Self, Self::Error> {
+        if args.contains(["-h", "--help"]) {
+            return Err(ArgsError::Help);
+        }
+
+        let day = match args.value_from_str("-d") {
+            Ok(day) => day,
+            Err(e) => {
+                return Err(ArgsError::Error(format!("{e}")));
+            }
+        };
+
+        Ok(Args {
+            day,
+            year: args
+                .value_from_str("-y")
+                .unwrap_or_else(|_| get_current_year()),
+        })
+    }
+}
