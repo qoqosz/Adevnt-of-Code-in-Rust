4c98d037df35c8de59def2b03e8e99ed481fafff
Łukasz Bednarski <lukasz@bednarski.me>
2023-12-04 11:43:45 +0100

    Remove redundant id

--- src/aoc2023/day4.rs
+++ src/aoc2023/day4.rs
@@ -3,8 +3,6 @@ use std::str::FromStr;
 
 #[derive(Debug)]
 struct Card {
-    // Card id
-    id: usize,
     // Count of winning numbers
     count: usize,
 }
@@ -13,8 +11,7 @@ impl FromStr for Card {
     type Err = std::num::ParseIntError;
 
     fn from_str(line: &str) -> Result<Self, Self::Err> {
-        let (header, content) = line.split_once(':').unwrap();
-        let id = header.trim_start_matches("Card").trim().parse::<usize>()?;
+        let (_, content) = line.split_once(':').unwrap();
         let (winning, owned) = content.split_once('|').unwrap();
 
         let winning_numbers = winning
@@ -24,7 +21,6 @@ impl FromStr for Card {
         let owned_numbers = owned.split(' ').flat_map(|n| n.trim().parse::<u8>());
 
         Ok(Card {
-            id,
             count: owned_numbers
                 .filter(|n| winning_numbers.contains(n))
                 .count(),
@@ -34,6 +30,7 @@ impl FromStr for Card {
 
 impl Card {
     // Score for Part I
+    #[inline]
     fn score(&self) -> u32 {
         (1 << self.count) >> 1
     }
@@ -41,18 +38,18 @@ impl Card {
 
 // Score for Part II
 fn recursive_score(id: usize, cards: &[Card], cache: &mut [u32]) -> u32 {
-    match cache.get(id - 1) {
+    match cache.get(id) {
         None | Some(&u32::MAX) => {}
         Some(score) => return *score,
     }
 
-    let card = &cards[id - 1];
+    let card = &cards[id];
     let score = 1
         + (1..=card.count)
             .map(|i| recursive_score(id + i, cards, cache))
             .sum::<u32>();
 
-    cache[id - 1] = score;
+    cache[id] = score;
     score
 }
 
@@ -68,7 +65,8 @@ pub fn main() {
     let mut cache: Vec<u32> = vec![u32::MAX; cards.len()];
     let recursive_score: u32 = cards
         .iter()
-        .map(|card| recursive_score(card.id, &cards, &mut cache))
+        .enumerate()
+        .map(|(id, _)| recursive_score(id, &cards, &mut cache))
         .sum();
     println!("{recursive_score}");
 }
