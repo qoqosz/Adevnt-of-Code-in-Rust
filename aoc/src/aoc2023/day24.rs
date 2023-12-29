52108271ef1df90e862b3ca840999b703d924c5f
Łukasz Bednarski <lukasz@bednarski.me>
2023-12-29 18:53:28 +0100

    Day 24, 2023

--- aoc/src/aoc2023/day24.rs
+++ aoc/src/aoc2023/day24.rs
@@ -1,14 +1,32 @@
+#![allow(non_snake_case)]
+/// Part 2
+///
+/// (r, v) - unknonwn
+///
+/// Because
+///  r_i + v_i t = r + v t => (r_i - r) = (v - v_i) t
+///
+/// we have
+///  0 = (r - r_i) ^ (v - v_i)
+///
+/// r ^ v = const, so from a pair of eqs for i and j:
+///
+///  0 = r ^ (v_j - v_i) ^ (r_j - r_i) + r_i ^ v_i ^ r_j + r_j ^ v_j ^ r_i
+///
+/// Having 3 such eqs allow to solve for all coordinates of r
 use aoc::{aoc, aoc_input};
 use itertools::Itertools;
 use std::fmt;
 
+type Vec3 = (f64, f64, f64);
+
 struct Hailstone {
-    x: i64,
-    y: i64,
-    z: i64,
-    vx: i64,
-    vy: i64,
-    vz: i64,
+    x: f64,
+    y: f64,
+    z: f64,
+    vx: f64,
+    vy: f64,
+    vz: f64,
 }
 
 impl fmt::Debug for Hailstone {
@@ -23,20 +41,12 @@ impl fmt::Debug for Hailstone {
 
 impl Hailstone {
     fn eval(&self, t: f64) -> (f64, f64) {
-        (
-            self.x as f64 + self.vx as f64 * t,
-            self.y as f64 + self.vy as f64 * t,
-        )
+        (self.x + self.vx * t, self.y + self.vy * t)
     }
 
     fn cross(&self, other: &Hailstone) -> f64 {
-        let (a, b) = (self.x as f64, self.y as f64);
-        let (x, y) = (self.vx as f64, self.vy as f64);
-        let (p, w) = (other.x as f64, other.y as f64);
-        let (q, r) = (other.vx as f64, other.vy as f64);
-
-        let num = (w - b) + r / q * (a - p);
-        let den = y - r / q * x;
+        let num = (other.y - self.y) + other.vy / other.vx * (self.x - other.x);
+        let den = self.vy - other.vy / other.vx * self.vx;
 
         num / den
     }
@@ -53,13 +63,13 @@ fn parse(data: &str) -> Vec<Hailstone> {
     {
         let (x, y, z) = pos
             .split(',')
-            .map(|x| x.trim().parse::<i64>().unwrap())
+            .map(|x| x.trim().parse::<f64>().unwrap())
             .tuples::<(_, _, _)>()
             .next()
             .unwrap();
         let (vx, vy, vz) = vel
             .split(',')
-            .map(|x| x.trim().parse::<i64>().unwrap())
+            .map(|x| x.trim().parse::<f64>().unwrap())
             .tuples::<(_, _, _)>()
             .next()
             .unwrap();
@@ -89,14 +99,100 @@ fn count_intersections2d(hailstones: &[Hailstone], min: f64, max: f64) -> usize 
 
         if min <= x && x <= max && min <= y && y <= max && t > 0.0 && s > 0.0 {
             n += 1;
-
-            // println!("{:?}\n{:?}", left, right);
-            // println!("{:?} {}, --- {}, {}", x, y, t, s);
-            // println!("{:?}\n\n", right.eval(t));
         }
     }
 
     n
+}
+
+// TODO: Implement Vec3d struct + all needed funcs
+fn multiply(a: f64, u: Vec3) -> Vec3 {
+    (a * u.0, a * u.1, a * u.2)
+}
+
+fn add(u: Vec3, v: Vec3) -> Vec3 {
+    (u.0 + v.0, u.1 + v.1, u.2 + v.2)
+}
+
+fn subtract(u: Vec3, v: Vec3) -> Vec3 {
+    (u.0 - v.0, u.1 - v.1, u.2 - v.2)
+}
+
+fn dot(u: Vec3, v: Vec3) -> f64 {
+    u.0 * v.0 + u.1 * v.1 + u.2 * v.2
+}
+
+fn cross(v: Vec3, w: Vec3) -> Vec3 {
+    (
+        v.1 * w.2 - v.2 * w.1,
+        v.2 * w.0 - v.0 * w.2,
+        v.0 * w.1 - v.1 * w.0,
+    )
+}
+
+// Coefficients in a row of: Ax = b
+fn get_coeffs(ri: Vec3, vi: Vec3, rj: Vec3, vj: Vec3) -> (Vec3, f64) {
+    let a = cross(subtract(vj, vi), subtract(rj, ri));
+    let b = dot(ri, cross(vi, rj)) + dot(rj, cross(vj, ri));
+    (a, -b)
+}
+
+fn gauss_elim(A: &[Vec3; 3], b: Vec3) -> Vec3 {
+    let mut A: [Vec3; 3] = *A;
+    let mut b = b;
+
+    // 1st round
+    let (a0, a1, a2) = (A[0].0, A[1].0, A[2].0);
+    A[1] = subtract(A[1], multiply(a1 / a0, A[0]));
+    A[2] = subtract(A[2], multiply(a2 / a0, A[0]));
+    b.1 -= a1 / a0 * b.0;
+    b.2 -= a2 / a0 * b.0;
+
+    // 2nd round
+    let (b1, b2) = (A[1].1, A[2].1);
+    A[2] = subtract(A[2], multiply(b2 / b1, A[1]));
+    b.2 -= b2 / b1 * b.1;
+
+    // solve
+    let z = b.2 / A[2].2;
+    let y = (b.1 - A[1].2 * z) / A[1].1;
+    let x = (b.0 - A[0].1 * y - A[0].2 * z) / A[0].0;
+
+    (x, y, z)
+}
+
+fn find_single_pos(r: &[Vec3; 6], v: &[Vec3; 6]) -> Vec3 {
+    let (A0, b0) = get_coeffs(r[0], v[0], r[1], v[1]);
+    let (A1, b1) = get_coeffs(r[2], v[2], r[3], v[3]);
+    let (A2, b2) = get_coeffs(r[4], v[4], r[5], v[5]);
+
+    let A = [A0, A1, A2];
+    let b = (b0, b1, b2);
+
+    gauss_elim(&A, b)
+}
+
+fn find_pos(hailstones: &[Hailstone]) -> Option<u64> {
+    let results = hailstones
+        .windows(6)
+        .map(|w| {
+            let (mut r, mut v) = (vec![], vec![]);
+            for h in w {
+                r.push((h.x, h.y, h.z));
+                v.push((h.vx, h.vy, h.vz));
+            }
+            find_single_pos(&r.try_into().unwrap(), &v.try_into().unwrap())
+        })
+        .take(30)
+        .collect::<Vec<Vec3>>();
+
+    // average over 30 hailstones
+    let r0 = *results.first()?;
+    let adj: Vec3 = results[1..].iter().map(|&r| subtract(r0, r)).reduce(add)?;
+
+    let res = subtract(r0, multiply(1.0 / results.len() as f64, adj));
+
+    Some(res.0.round() as u64 + res.1.round() as u64 + res.2.round() as u64)
 }
 
 #[aoc(2023, 24)]
@@ -105,11 +201,13 @@ pub fn main() {
     let hailstones = parse(&data);
 
     // Part I
-    let min = 200000000000000.0;
-    let max = 400000000000000.0;
+    let min = 200_000_000_000_000.0;
+    let max = 400_000_000_000_000.0;
     println!("{}", count_intersections2d(&hailstones, min, max));
 
     // Part II
+    let r = find_pos(&hailstones).unwrap();
+    println!("{}", r);
 }
 
 #[cfg(test)]
@@ -126,11 +224,47 @@ mod tests {
     fn test_part1() {
         let hailstones = parse(EXAMPLE);
         let z = count_intersections2d(&hailstones, 7.0, 27.0);
-        println!("{}", z);
+        assert_eq!(z, 2);
     }
 
     #[test]
-    fn test_part2() {
-        //        assert_eq!(part2(&lines), 1);
+    fn test_multiply() {
+        let v = (13.0, 0.0, -9.0);
+        let v2 = multiply(2.0, v);
+        assert_eq!(v2, (26.0, 0.0, -18.0));
+    }
+
+    #[test]
+    fn test_subtract() {
+        let u = (90.0, -18.0, 7.0);
+        let v = (20.0, 16.6, 3.4);
+        let expected = (70.0, -34.6, 3.6);
+        assert_eq!(subtract(u, v), expected);
+    }
+
+    #[test]
+    fn test_dot() {
+        let u = (17.0, 3.4, 0.0);
+        let v = (9.1, 2.9, 2.3);
+        let expected = 164.56;
+        assert_eq!(dot(u, v), expected);
+    }
+
+    #[test]
+    fn test_cross() {
+        let u = (1.0, 2.0, 3.0);
+        let v = (4.0, 5.0, 6.0);
+        let expected = (-3.0, 6.0, -3.0);
+        assert_eq!(cross(u, v), expected);
+    }
+
+    // Solve[{3x+2y+z==39, 2x+3y+z==34, x+2y+3z=26}, {x, y, z}]
+    // x = 37/4 and y = 17/4 and z = 11/4
+    #[test]
+    fn test_gauss_elim() {
+        let A = [(3.0, 2.0, 1.0), (2.0, 3.0, 1.0), (1.0, 2.0, 3.0)];
+        let b = (39.0, 34.0, 26.0);
+        let expected = (9.25, 4.25, 2.75);
+        assert_eq!(gauss_elim(&A, b), expected);
     }
 }
