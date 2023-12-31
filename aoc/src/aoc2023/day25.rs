f12ec729f6b7659063f213f02d8640da7d97e100
Łukasz Bednarski <lukasz@bednarski.me>
2023-12-31 12:03:58 +0100

    Rework 25, 2023

--- aoc/src/aoc2023/day25.rs
+++ aoc/src/aoc2023/day25.rs
@@ -1,98 +1,22 @@
 use aoc::{aoc, aoc_input};
-use rand::seq::SliceRandom;
-use rustc_hash::{FxHashMap, FxHashSet};
-use std::fmt::{Debug, Display};
-use std::hash::Hash;
+use rustworkx_core::connectivity::stoer_wagner_min_cut;
+use rustworkx_core::petgraph::graphmap::UnGraphMap;
 
-type Graph<T> = FxHashMap<T, Vec<T>>;
-
-trait UnGraph<T> {
-    fn nodes(&self) -> FxHashSet<T>;
-    fn edges(&self) -> Vec<(T, T)>;
+fn parse(data: &str) -> UnGraphMap<&str, ()> {
+    let edges = data.trim().lines().flat_map(|line| {
+        let (key, vals) = line.split_once(": ").unwrap();
+        vals.split(' ').map(move |val| (key, val))
+    });
+    UnGraphMap::<&str, ()>::from_edges(edges)
 }
 
-impl<T> UnGraph<T> for Graph<T>
-where
-    T: Eq + PartialEq + Hash + Copy,
-{
-    fn nodes(&self) -> FxHashSet<T> {
-        self.iter().fold(FxHashSet::default(), |mut acc, x| {
-            acc.insert(*x.0);
-            acc.extend(x.1);
-            acc
-        })
-    }
+fn solve(graph: &UnGraphMap<&str, ()>) -> Option<usize> {
+    let cut: rustworkx_core::Result<Option<(usize, Vec<_>)>> =
+        stoer_wagner_min_cut(&graph, |_| Ok(1));
+    let (_, partition) = cut.ok()??;
+    let (n, m) = (graph.node_count(), partition.len());
 
-    fn edges(&self) -> Vec<(T, T)> {
-        self.iter()
-            .flat_map(|(k, v)| v.iter().map(|x| (*k, *x)))
-            .collect::<Vec<_>>()
-    }
-}
-
-fn contract<T>(graph: &mut Graph<T>, u: T, v: T)
-where
-    T: Eq + PartialEq + Hash + Copy,
-{
-    if let Some(vals) = graph.remove(&v) {
-        graph.entry(u).and_modify(|e| e.extend(vals));
-    }
-
-    for (k, w) in graph.iter_mut() {
-        w.iter_mut().for_each(|node| {
-            if *node == v {
-                *node = u
-            }
-        });
-        w.retain(|node| *node != *k);
-    }
-
-    graph.retain(|_, w| !w.is_empty());
-}
-
-fn karger<T>(graph: &mut Graph<T>) -> (usize, usize, usize)
-where
-    T: Eq + PartialEq + Hash + Copy,
-{
-    let mut rng = &mut rand::thread_rng();
-    let mut weights: FxHashMap<T, usize> = FxHashMap::default();
-    let n = graph.nodes().len();
-
-    while graph.nodes().len() > 2 {
-        let (u, v) = *graph.edges().choose(&mut rng).unwrap();
-        contract(graph, u, v);
-
-        let weight = weights.remove(&v).unwrap_or(1);
-        *weights.entry(u).or_insert(1) += weight;
-    }
-
-    let i = *weights.iter().next().unwrap().1;
-    let n_cuts: usize = graph.values().map(|v| v.len()).sum();
-    (n_cuts, n - i, i)
-}
-
-fn solve<T>(graph: &Graph<T>) -> usize
-where
-    T: Eq + PartialEq + Hash + Copy + Debug + Display,
-{
-    loop {
-        let (n_cuts, a, b) = karger(&mut graph.clone());
-
-        if n_cuts == 3 {
-            return a * b;
-        }
-    }
-}
-
-fn parse(data: &str) -> Graph<&str> {
-    let mut graph: Graph<&str> = FxHashMap::default();
-
-    for line in data.trim().lines() {
-        let (key, vals) = line.rsplit_once(": ").unwrap();
-        graph.insert(key, vals.split(' ').collect::<Vec<_>>());
-    }
-
-    graph
+    Some(m * (n - m))
 }
 
 #[aoc(2023, 25)]
@@ -101,7 +25,7 @@ pub fn main() {
     let graph = parse(&data);
 
     // Part I
-    println!("{}", solve(&graph));
+    println!("{}", solve(&graph).unwrap());
 }
 
 #[cfg(test)]
@@ -125,6 +49,6 @@ frs: qnr lhk lsr";
     #[test]
     fn test_part1() {
         let graph = parse(EXAMPLE);
-        assert_eq!(solve(&graph), 54);
+        assert_eq!(solve(&graph), Some(54));
     }
 }
