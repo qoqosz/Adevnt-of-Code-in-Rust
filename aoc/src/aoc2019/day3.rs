39ce1be8cda4bb7f5ddf0a9291c2c659efbb6d55
Łukasz Bednarski <lukasz@bednarski.me>
2024-12-13 09:15:01 +0100

    Day 13, 2024

--- aoc/src/aoc2019/day3.rs
+++ aoc/src/aoc2019/day3.rs
@@ -1,6 +1,60 @@
 use aoc::{aoc, aoc_input};
+use glam::I16Vec2 as Point;
+use rustc_hash::{FxHashMap, FxHashSet};
+
+fn parse(data: &str) -> (Vec<&str>, Vec<&str>) {
+    let (left, right) = data.trim().split_once('\n').unwrap();
+    (left.split(',').collect(), right.split(',').collect())
+}
+
+fn make_wire(path: &Vec<&str>) -> FxHashMap<Point, usize> {
+    let (mut i, mut c) = (1, Point::new(0, 0));
+    let mut covered = FxHashMap::default();
+
+    for step in path {
+        let (dir, len) = step.split_at(1);
+        let n: usize = len.parse().unwrap();
+        let ds = match dir {
+            "R" => Point::new(1, 0),
+            "L" => Point::new(-1, 0),
+            "U" => Point::new(0, 1),
+            "D" => Point::new(0, -1),
+            _ => unreachable!(),
+        };
+
+        for _ in 0..n {
+            c += ds;
+
+            if !covered.contains_key(&c) {
+                covered.insert(c, i);
+            }
+
+            i += 1;
+        }
+    }
+
+    covered
+}
 
 #[aoc(2019, 3)]
 pub fn main() {
-    let _data = aoc_input!(2019, 3).unwrap();
+    let data = aoc_input!(2019, 3).unwrap();
+    let (left, right) = parse(&data);
+    let (wire1, wire2) = (make_wire(&left), make_wire(&right));
+    let crossings = wire1
+        .keys()
+        .filter(|k| wire2.contains_key(k))
+        .collect::<FxHashSet<_>>();
+
+    // Part I
+    let dist = crossings
+        .iter()
+        .map(|c| c.x.abs() + c.y.abs())
+        .min()
+        .unwrap();
+    println!("{dist}");
+
+    // Part II
+    let steps = crossings.iter().map(|c| wire1[c] + wire2[c]).min().unwrap();
+    println!("{steps}");
 }
---
+++ aoc/src/aoc2024/day13.rs
@@ -0,0 +1,81 @@
+use aoc::{aoc, aoc_input};
+use glam::I64Vec2 as Coord;
+use regex_lite::Regex;
+use std::str::FromStr;
+use std::sync::LazyLock;
+
+static RE: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\d+").unwrap());
+static EPSILON: f64 = 1e-6;
+
+struct Machine {
+    a: Coord,
+    b: Coord,
+    prize: Coord,
+}
+
+impl Machine {
+    fn shift(&self, ds: i64) -> Self {
+        Self {
+            prize: self.prize + Coord::new(ds, ds),
+            ..*self
+        }
+    }
+
+    fn solve(&self) -> Option<usize> {
+        let (a, b, p) = (self.a.as_dvec2(), self.b.as_dvec2(), self.prize.as_dvec2());
+        let det = a.x * b.y - a.y * b.x;
+
+        if det.abs() < EPSILON {
+            return None;
+        }
+
+        let a_coeff = (b.y * p.x - b.x * p.y) / det;
+        let b_coeff = (a.x * p.y - a.y * p.x) / det;
+
+        // solution must be a non-negative integer
+        if (a_coeff < 0.0)
+            || (b_coeff < 0.0 || a_coeff.fract() > EPSILON || b_coeff.fract() > EPSILON)
+        {
+            None
+        } else {
+            Some((3.0 * a_coeff + b_coeff) as usize)
+        }
+    }
+}
+
+impl FromStr for Machine {
+    type Err = ();
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let nums: Vec<i64> = RE
+            .find_iter(data)
+            .filter_map(|digits| digits.as_str().parse().ok())
+            .collect();
+        Ok(Machine {
+            a: Coord::new(nums[0], nums[1]),
+            b: Coord::new(nums[2], nums[3]),
+            prize: Coord::new(nums[4], nums[5]),
+        })
+    }
+}
+
+#[aoc(2024, 13)]
+pub fn main() {
+    let data = aoc_input!(2024, 13).unwrap();
+    let machines = data
+        .trim()
+        .split("\n\n")
+        .flat_map(|section| Machine::from_str(section).ok())
+        .collect::<Vec<_>>();
+
+    // Part I
+    let tokens = machines.iter().filter_map(|m| m.solve()).sum::<usize>();
+    println!("{tokens}");
+
+    // Part II
+    let tokens = machines
+        .iter()
+        .filter_map(|m| m.shift(10000000000000).solve())
+        .sum::<usize>();
+    println!("{tokens}");
+}
