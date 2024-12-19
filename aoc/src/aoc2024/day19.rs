4d729133ce3cd9cf4bffc1d2e6d1504812b60187
Łukasz Bednarski <lukasz@bednarski.me>
2024-12-19 08:44:22 +0100

    Day 19, 2024

---
+++ aoc/src/aoc2024/day19.rs
@@ -0,0 +1,74 @@
+use aoc::{aoc, aoc_input};
+use itertools::Itertools;
+use rustc_hash::FxHashMap;
+
+trait TrimOnceStartMatches {
+    fn trim_once_start_matches(&self, pat: &str) -> &Self;
+}
+
+impl TrimOnceStartMatches for str {
+    fn trim_once_start_matches(&self, pat: &str) -> &Self {
+        if self.starts_with(pat) {
+            &self[pat.len()..]
+        } else {
+            self
+        }
+    }
+}
+
+fn parse(data: &str) -> (Vec<&str>, Vec<&str>) {
+    let (towels, patterns) = data.trim().split_once("\n\n").unwrap();
+
+    (
+        towels
+            .split(", ")
+            .sorted_by_key(|x| x.len())
+            .rev()
+            .collect(),
+        patterns.lines().collect(),
+    )
+}
+
+fn check_design<'a>(
+    pattern: &'a str,
+    towels: &[&str],
+    cache: &mut FxHashMap<&'a str, usize>,
+) -> usize {
+    if let Some(ans) = cache.get(pattern) {
+        return *ans;
+    }
+
+    let n = pattern.len();
+
+    if n == 0 {
+        return 1;
+    }
+
+    let val = towels
+        .iter()
+        .map(|towel| pattern.trim_once_start_matches(towel))
+        .filter(|p| p.len() < n)
+        .map(|p| check_design(p, towels, cache))
+        .sum();
+
+    cache.insert(pattern, val).unwrap_or(val)
+}
+
+#[aoc(2024, 19)]
+pub fn main() {
+    let data = aoc_input!(2024, 19).unwrap();
+    let (towels, patterns) = parse(&data);
+    let mut cache = FxHashMap::default();
+    let designs: Vec<_> = patterns
+        .iter()
+        .map(|p| check_design(p, &towels, &mut cache))
+        .collect();
+
+    // Part I
+    let n_designs: usize = designs.iter().filter(|n| **n > 0).count();
+    println!("{n_designs}");
+
+    // Part II
+    let n_ways: usize = designs.iter().sum();
+    println!("{n_ways}");
+}
