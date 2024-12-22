ceb0cf0e91c1d27ae2735d28ecc6f9b15a0c62f2
Łukasz Bednarski <lukasz@bednarski.me>
2024-12-22 12:31:04 +0100

    Day 22, 2024

---
+++ aoc/src/aoc2024/day22.rs
@@ -0,0 +1,105 @@
+use aoc::{aoc, aoc_input};
+use rustc_hash::FxHashMap;
+use std::iter::successors;
+
+#[inline(always)]
+fn mix(secret: usize, other: usize) -> usize {
+    secret ^ other
+}
+
+#[inline(always)]
+fn prune(secret: usize) -> usize {
+    secret % 16777216
+}
+
+fn next_secret(secret: usize) -> usize {
+    // Step 1
+    let mut secret = prune(mix(secret, secret * 64));
+    // Step 2
+    secret = prune(mix(secret, secret / 32));
+    // Step 3
+    prune(mix(secret, secret * 2048))
+}
+
+#[aoc(2024, 22)]
+pub fn main() {
+    let data = aoc_input!(2024, 22).unwrap();
+    let secrets: Vec<usize> = data
+        .trim()
+        .lines()
+        .filter_map(|line| line.parse().ok())
+        .collect();
+
+    // Part I
+    let buyers_secrets = secrets
+        .iter()
+        .map(|&secret| {
+            successors(Some(secret), |x| Some(next_secret(*x)))
+                .take(2001)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let sum_secrets = buyers_secrets
+        .iter()
+        .filter_map(|bs| bs.last())
+        .copied()
+        .sum::<usize>();
+    println!("{sum_secrets}");
+
+    // Part II
+    let buyers_prices = buyers_secrets
+        .iter()
+        .map(|bs| bs.iter().map(|x| (x % 10) as i64).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let bs_maps = buyers_prices
+        .iter()
+        .map(|bs| {
+            let mut price_map = FxHashMap::default();
+
+            for win in bs.windows(5) {
+                let diff = (
+                    win[1] - win[0],
+                    win[2] - win[1],
+                    win[3] - win[2],
+                    win[4] - win[3],
+                );
+
+                if !price_map.contains_key(&diff) {
+                    price_map.insert(diff, (win[4] % 10) as i64);
+                }
+            }
+
+            price_map
+        })
+        .collect::<Vec<_>>();
+
+    let mut res_map = FxHashMap::default();
+
+    for bs_map in bs_maps {
+        for (diff, price) in bs_map {
+            *res_map.entry(diff).or_insert(0) += price;
+        }
+    }
+
+    println!("{}", res_map.values().max().unwrap());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mix() {
+        assert_eq!(37, mix(42, 15));
+    }
+
+    #[test]
+    fn test_prune() {
+        assert_eq!(16113920, prune(100000000));
+    }
+
+    #[test]
+    fn test_next_secret() {
+        assert_eq!(15887950, next_secret(123));
+    }
+}
