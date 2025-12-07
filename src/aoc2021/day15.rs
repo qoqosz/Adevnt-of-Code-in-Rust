a36093117c9dd744f12f9c3d0dfd68b8e5326c1a
Łukasz Bednarski <lukasz@bednarski.me>
2025-12-07 11:23:17 +0100

    Tidy up code for day 15, 2021

--- src/aoc2021/day15.rs
+++ src/aoc2021/day15.rs
@@ -1,55 +1,49 @@
+use aoc::aoc_input;
 use aoc::graph::parse_graph;
-use aoc::load_input;
-use aoc_cache::get;
 use petgraph::algo::{astar, Measure};
-use petgraph::graph::{DiGraph, NodeIndex};
+use petgraph::graph::DiGraph;
 use petgraph::visit::EdgeRef;
 use std::fmt::Debug;
-
-#[allow(dead_code)]
-static TEST_CASE: &'static str = "1163751742
-1381373672
-2136511328
-3694931569
-7463417111
-1319128137
-1359912421
-3125421639
-1293138521
-2311944581";
 
 fn shortest_path<T>(graph: &DiGraph<T, ()>) -> Option<T>
 where
     T: Copy + Debug + Default + Measure,
 {
-    let start = graph.node_indices().nth(0).unwrap();
+    let start = graph.node_indices().next().unwrap();
     let goal = graph.node_indices().last().unwrap();
     let path = astar(
         &graph,
         start,
         |f| f == goal,
-        |e| {
-            let idx: NodeIndex = e.target().into();
-            graph[idx]
-        },
+        |e| graph[e.target()],
         |_| T::default(),
     );
     path.unzip().0
 }
 
 fn main() {
-    const MY_COOKIE: &str = include_str!("session.cookie");
-    let data =
-        load_input!("/Users/qoqosz/Documents/Coding/Rust/Advent of Code/data/2021/day15.txt");
+    let data = aoc_input!(2021, 15).unwrap();
     let graph: DiGraph<u32, ()> = parse_graph(&data, |c| c.parse::<u32>().unwrap());
-    let dist = shortest_path(&graph).expect("Could not find the shortest path");
+    let dist = shortest_path(&graph).unwrap();
 
-    println!("{:?}", dist);
+    // Part I
+    println!("{}", dist);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    static TEST_CASE: &str = "1163751742\n\
+                              1381373672\n\
+                              2136511328\n\
+                              3694931569\n\
+                              7463417111\n\
+                              1319128137\n\
+                              1359912421\n\
+                              3125421639\n\
+                              1293138521\n\
+                              2311944581";
 
     #[test]
     fn test_part_1() {
