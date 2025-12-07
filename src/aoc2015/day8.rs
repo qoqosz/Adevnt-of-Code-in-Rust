f0c8cfa5666ebd3b5aa4af955a990709a471fd59
Łukasz Bednarski <lukasz@bednarski.me>
2025-12-07 11:22:15 +0100

    Day 8

--- src/aoc2015/day8.rs
+++ src/aoc2015/day8.rs
@@ -1,7 +1,60 @@
-fn main() {
-    let data = vec!["\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "0x27"];
+use aoc::load_input;
 
-    for x in data.iter() {
-        println!("{}", x.chars().count());
+pub trait InMemorySize {
+    fn in_mem_size(&self) -> usize;
+}
+
+impl InMemorySize for &str {
+    fn in_mem_size(&self) -> usize {
+        let mut input = self.chars().peekable();
+        let mut size: usize = 0;
+
+        loop {
+            match input.next() {
+                Some('"') => {}
+                Some('\\') => match input.peek() {
+                    Some('x') => {
+                        input.next();
+                        input.next();
+                        input.next();
+                        size += 1;
+                    }
+                    _ => {
+                        input.next();
+                        size += 1
+                    }
+                },
+                Some(_) => size += 1,
+                _ => break,
+            }
+        }
+        size
     }
+}
+
+fn encoded_len<T: Into<String>>(text: T) -> usize {
+    text.into().chars().fold(2, |sum, ch| match ch {
+        '"' | '\\' => sum + 2,
+        _ => sum + 1,
+    })
+}
+
+fn main() {
+    let data = load_input!("/Users/qoqosz/Documents/Coding/Rust/Advent of Code/data/2015/day8.txt");
+    let lines: Vec<_> = data.split('\n').filter(|&x| !x.is_empty()).collect();
+
+    // Part I
+    let mut n_code = 0;
+    let mut n_mem = 0;
+    let mut n_enc = 0;
+
+    for line in lines.into_iter() {
+        n_code += line.len();
+        n_mem += line.in_mem_size();
+        n_enc += encoded_len(line);
+    }
+    println!("{}", n_code - n_mem);
+
+    // Part II
+    println!("{}", n_enc - n_code);
 }
