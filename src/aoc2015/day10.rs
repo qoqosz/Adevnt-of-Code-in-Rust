a28c798281239b1b2b0682d03b3da4eb5a75173a
Łukasz Bednarski <lukasz@bednarski.me>
2025-12-07 11:22:15 +0100

    Day 10, 2015

---
+++ src/aoc2015/day10.rs
@@ -0,0 +1,38 @@
+use aoc::load_input;
+
+fn look_and_say(text: &String) -> String {
+    let mut out: Vec<String> = Vec::new();
+    let mut count = 0;
+    let mut current = text.chars().next().unwrap();
+
+    for ch in text.chars() {
+        if ch == current {
+            count += 1
+        } else {
+            out.push(format!("{}{}", count, current));
+            count = 1;
+            current = ch;
+        }
+    }
+    out.push(format!("{}{}", count, current));
+    out.join("")
+}
+
+fn play(text: &String, n: usize) -> usize {
+    let mut text = text.clone();
+
+    for i in 0..n {
+        text = look_and_say(&text);
+    }
+    text.len()
+}
+
+fn main() {
+    let data = "1321131112".to_string();
+
+    // Part I
+    println!("{}", play(&data, 40));
+
+    // Part II
+    println!("{}", play(&data, 50));
+}
