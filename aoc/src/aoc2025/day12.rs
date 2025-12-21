301ca1d1fe41471fc9a6f7e385995b676569df64
Łukasz Bednarski <lukasz@bednarski.me>
2025-12-21 19:15:19 +0100

    Day 12, 2025

--- aoc/src/aoc2025/day12.rs
+++ aoc/src/aoc2025/day12.rs
@@ -1,6 +1,24 @@
 use aoc::{aoc, aoc_input};
 
+fn parse(data: &str) -> (u32, u32, Vec<u32>) {
+    let w = data[..2].parse::<u32>().unwrap();
+    let h = data[3..5].parse::<u32>().unwrap();
+    let boxes = data[7..]
+        .split_whitespace()
+        .flat_map(|x| x.parse())
+        .collect::<Vec<_>>();
+    (w, h, boxes)
+}
+
 #[aoc(2025, 12)]
 pub fn main() {
     let data = aoc_input!(2025, 12).unwrap();
+    let input = data.trim().lines().skip(30);
+
+    // Part I
+    let n = input
+        .map(parse)
+        .filter(|(w, h, boxes)| (w / 3) * (h / 3) >= boxes.iter().sum())
+        .count();
+    println!("{n}");
 }
